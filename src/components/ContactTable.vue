0e9d9990acbd6ee1aaf0e42ece6bd82608cb99eb
Nathan Souza <ant.nathan15@gmail.com>
2021-12-06 16:34:44 -0300

    feature: add vuex to persist data

--- src/components/ContactTable.vue
+++ src/components/ContactTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-table class="table is-fullwidth" :data="data" narrowed hoverable>
+  <b-table class="table is-fullwidth" :data="contacts" narrowed hoverable>
     <b-table-column
       header-class="table-header"
       label=""
@@ -27,51 +27,22 @@
       :td-attrs="columnTdAttrs"
       numeric
       label=""
+      v-slot="props"
     >
       <img src="@/assets/ic-edit.svg" class="ic_edit" />
-      <img src="@/assets/ic-delete.svg" class="ic_delete" />
+      <img
+        src="@/assets/ic-delete.svg"
+        class="ic_delete"
+        @click="() => removeContact(props.row.id)"
+      />
     </b-table-column>
   </b-table>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-  data() {
-    return {
-      data: [
-        {
-          id: 1,
-          first_name: "Leanne",
-          email: "Sincere@april.biz",
-          phone: "1-770-736-8031",
-        },
-        {
-          id: 2,
-          first_name: "John",
-          email: "Shanna@melissa.tv",
-          phone: "010-692-6593",
-        },
-        {
-          id: 3,
-          first_name: "Tina",
-          email: "Nathan@yesenia.net",
-          phone: "1-463-123-4447",
-        },
-        {
-          id: 4,
-          first_name: "Clarence",
-          email: "Julianne.OConner@kory.org",
-          phone: "493-170-9623 x156",
-        },
-        {
-          id: 5,
-          first_name: "Anne",
-          email: "Lucio_Hettinger@annie.ca",
-          phone: "(254)954-1289",
-        },
-      ],
-    };
-  },
+  computed: mapState(["contacts"]),
   methods: {
     backgroundColor(letter) {
       const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -85,8 +56,6 @@ export default {
         "#5fe2c4",
         "#f55a5a",
       ];
-      console.log(letter);
-      console.log(alphabet.indexOf(letter));
       return {
         "background-color": colors[alphabet.indexOf(letter) % colors.length],
       };
@@ -97,6 +66,10 @@ export default {
           "justify-content": "center !important",
         },
       };
+    },
+    removeContact(id) {
+      console.log(id);
+      this.$store.commit("remove", id);
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,66 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import VueCookies from "vue-cookies";
 
 Vue.use(Vuex);
+Vue.use(VueCookies);
+Vue.$cookies.config("7d");
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    contacts: [
+      {
+        id: 1,
+        first_name: "Leanne",
+        email: "Sincere@april.biz",
+        phone: "1-770-736-8031",
+      },
+      {
+        id: 2,
+        first_name: "John",
+        email: "Shanna@melissa.tv",
+        phone: "010-692-6593",
+      },
+      {
+        id: 3,
+        first_name: "Tina",
+        email: "Nathan@yesenia.net",
+        phone: "1-463-123-4447",
+      },
+      {
+        id: 4,
+        first_name: "Clarence",
+        email: "Julianne.OConner@kory.org",
+        phone: "493-170-9623 x156",
+      },
+      {
+        id: 5,
+        first_name: "Anne",
+        email: "Lucio_Hettinger@annie.ca",
+        phone: "(254)954-1289",
+      },
+    ],
+  },
+  mutations: {
+    push(state, contact) {
+      state.contacts.push(contact);
+    },
+    remove(state, id) {
+      console.log(id);
+      const index = state.contacts.findIndex((contact) => contact.id === id);
+      state.contacts.splice(index, 1);
+    },
+  },
   actions: {},
   modules: {},
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => Vue.$cookies.get(key),
+        setItem: (key, value) => Vue.$cookies.set(key, value),
+        removeItem: (key) => Vue.$cookies.remove(key),
+      },
+    }),
+  ],
 });
